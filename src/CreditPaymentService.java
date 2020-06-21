2a8404d29236b1692f64ebffd5749c2997e02fb8
Timur Kiselev <snygens@mail.ru>
2020-06-21 14:41:06 +0300

    форматирование кода

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,35 +1,12 @@
-import java.util.Scanner;
+
 import java.lang.Math;
+
 public class CreditPaymentService {
-    public double calculate (double credit, int mounths, double percent)
-    {
+    public double calculate(double credit, int mounths, double percent) {
         double amount = 0;
         double j = percent / 100 / 12;
-        double coefficient = j * Math.pow ((1 + j), mounths) / (Math.pow ((1 + j), mounths) - 1);
+        double coefficient = j * Math.pow((1 + j), mounths) / (Math.pow((1 + j), mounths) - 1);
         amount = coefficient * credit;
-        // double monthlypayment = 0;
-
         return amount;
-       /*-------------------------------------------------------------------------------------------*/
-      /*  double percentmouth = 0; //bodyoftheloan  * percent / 365 * 30;
-        //float bodyoftheloan;
-        //float monthlypayment = credit / mounths;
-        //System.out.print ("\nПервый месяц выплота составляет \t " + itog);
-        // System.out.print ("\nОсновной долг в месяц \t " + monthlypayment);
-        for (int i = mounths; i > 0; i --)
-        {
-            credit -= monthlypayment;
-            percentmouth = credit * percent / 365 * 30;
-
-            itog = monthlypayment +  percentmouth;
-            System.out.print ("\nЕжемесячный платеж \t " + itog + "\t Процентная ставка \t" + percentmouth);
-        }
-
-        /*-------------------------------------------------------------------------------------------*/
-
-
-
-
     }
 }
-//https://myfin.by/wiki/term/differencirovannye-platezhi
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         Scanner in = new Scanner(System.in);
-        System.out.print ("Введите сумму кредита : \t");
+        System.out.print("Введите сумму кредита : \t");
         float credit = in.nextFloat();
-        System.out.print ("Введите срок кредитования : \t");
+        System.out.print("Введите срок кредитования : \t");
         int mounths = in.nextInt();
-        System.out.print ("Введите процентную ставку : \t");
+        System.out.print("Введите процентную ставку : \t");
         double percent = in.nextFloat();
-        System.out.print (": \n" + service.calculate (credit, mounths, percent));
+        System.out.print(": \n" + service.calculate(credit, mounths, percent));
     }
 }
-// сложнАААА, без помощи не обошелся
+
